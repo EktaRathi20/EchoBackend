8b7e3ca4142b4fa448ea9ed0725a3dbe30cbdf08
Ekta <85895819+EktaRathi20@users.noreply.github.com>
2023-11-24 15:50:37 +0530

    Update RouteMiddleware.ts related to access token logic

--- src/middleware/RouteMiddleware.ts
+++ src/middleware/RouteMiddleware.ts
@@ -1,5 +1,22 @@
 import express from 'express';
-
+// import { verifyAccessToken } from '../utility/verifyToken'; 
 export const routeMiddleware  = (request : express.Request, response : express.Response, next:express.NextFunction ) => {
+    // TODO - just added the logic related to the access token 
+
+    /*
+        const token = request.accessToken;
+        if (!token) return response.status(401).json({ error: "Unauthorized: Token is missing." });
+        try{
+            const user = await verifyAccessToken(token);
+            if (user) {
+                req.user = user;
+                next(); // Proceed to the next middleware or route handler
+            } else {
+              return response.status(403).json({ error: 'Forbidden: Token is invalid.' });
+            }
+        } catch (error) {
+            return res.status(403).json({ error: 'Forbidden: Token is invalid.' });
+        }
+    */
     next();
 }
